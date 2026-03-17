1d3aac82673926c1d82e743101e9fc8613238442
Aykut <aykut.adem.0@gmail.com>
2026-03-17 04:17:32 +0300

    ML.NET integration completed

---
+++ SalesVisionAI.WebUI/Dtos/LoyaltyDtos/LoyaltyScoreDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SalesVisionAI.WebUI.Dtos.LoyaltyDtos
+{
+    public class LoyaltyScoreDto
+    {
+        public string CustomerName { get; set; }
+        public int TotalOrders { get; set; }
+        public decimal TotalSpent { get; set; }
+        public DateTime? LastOrderDate { get; set; }
+        public double LoyaltyScore { get; set; }
+    }
+}
---
+++ SalesVisionAI.WebUI/Dtos/LoyaltyMLDtos/LoyaltyScoreMLPredictionDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace SalesVisionAI.WebUI.Dtos.LoyaltyMLDtos
+{
+    public class LoyaltyScoreMLPredictionDto
+    {
+        [ColumnName("Score")]
+        public float LoyaltyScore { get; set; }
+    }
+}
